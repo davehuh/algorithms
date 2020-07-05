f15e7dfa45eb0ce9b920a97b2b2146bf0bd60045
Dave Huh <dave.huh11@gmail.com>
2020-07-05 02:43:58 -0500

    finished sort by using last element as pivot

--- week3/quicksort.py
+++ week3/quicksort.py
@@ -32,11 +32,14 @@ class QuickSort:
         last_pivot, comp_count_last = \
             self.quicksort_choose_last_as_pivot(ul.copy())
 
+        median_pivot, comp_count_median = \
+            self.quicksort_choose_median_as_pivot(ul.copy())
+
         # TODO
 #        print("final first pivot: ", first_pivot)
         print("first as pivot, counts: ", comp_count_first)
 
-        print("final last pivot: ", last_pivot)
+#        print("final last pivot: ", last_pivot)
         print("last as pivot, counts: ", comp_count_last)
 
         return last_pivot
@@ -65,6 +68,7 @@ class QuickSort:
         Sort using first element in list as the pivot
         Input:
             ul: unsorted list
+            which_as_pivot: Which element as pivot: "first" "last" ""
         Output:
             sorted list, ul
             comp_count is the number of comparisons
@@ -135,14 +139,23 @@ class QuickSort:
 
         pivot_idx = len(ul) - 1
         pivot = ul[pivot_idx]
-        START_IDX = 0
-        END_IDX = len(ul) - 1
+
+        START_IDX = 1
         idx_include_greater_than_pivot = START_IDX
 
         # TODO
-        print("before sorting: ", ul)
+#        print("before sorting: ", ul)
 
-        for idx in range(START_IDX, END_IDX):
+        # Swap pivot to first element to ensure same comparison counts
+        tmp = ul[0]
+        ul[0] = pivot
+        ul[pivot_idx] = tmp
+        pivot_idx = 0
+
+        # TODO
+#        print("swapped pivot: ", ul)
+
+        for idx in range(START_IDX, len(ul)):
             if ul[idx] < pivot and idx != idx_include_greater_than_pivot:
                 tmp = ul[idx_include_greater_than_pivot]
                 ul[idx_include_greater_than_pivot] = ul[idx]
@@ -152,15 +165,15 @@ class QuickSort:
                 idx_include_greater_than_pivot += 1
 
         # TODO
-        print("after sorting: ", ul)
+#        print("after sorting: ", ul)
 
-        new_pivot_idx = idx_include_greater_than_pivot
+        new_pivot_idx = idx_include_greater_than_pivot - 1
         tmp = ul[new_pivot_idx]
         ul[new_pivot_idx] = pivot
         ul[pivot_idx] = tmp
 
         # TODO
-        print("after swapping pivot:", ul)
+#        print("after swapping pivot:", ul)
 
         comp_count = len(ul) - 1
 
@@ -178,6 +191,57 @@ class QuickSort:
             comp_count += tmp_count_right
 
         return ul, comp_count
+
+#        if len(ul) < 2:
+#            return ul, 0
+#
+#        pivot_idx = len(ul) - 1
+#        pivot = ul[pivot_idx]
+#        START_IDX = 0
+#        END_IDX = len(ul) - 1
+#        idx_include_greater_than_pivot = START_IDX
+#
+#        # TODO
+#        print("before sorting: ", ul)
+#
+#        for idx in range(START_IDX, END_IDX):
+#            if ul[idx] < pivot and idx != idx_include_greater_than_pivot:
+#                tmp = ul[idx_include_greater_than_pivot]
+#                ul[idx_include_greater_than_pivot] = ul[idx]
+#                ul[idx] = tmp
+#                idx_include_greater_than_pivot += 1
+#            elif ul[idx] < pivot:
+#                idx_include_greater_than_pivot += 1
+#
+#        # TODO
+#        print("after sorting: ", ul)
+#
+#        new_pivot_idx = idx_include_greater_than_pivot
+#        tmp = ul[new_pivot_idx]
+#        ul[new_pivot_idx] = pivot
+#        ul[pivot_idx] = tmp
+#
+#        # TODO
+#        print("after swapping pivot:", ul)
+#
+#        comp_count = len(ul) - 1
+#
+#        # Remaining partitions need sorting
+#        # Check left
+#        if new_pivot_idx > 1:
+#            ul[:new_pivot_idx], tmp_count_left = \
+#                self.quicksort_choose_last_as_pivot(ul[:new_pivot_idx])
+#            comp_count += tmp_count_left
+#
+#        # Check right
+#        if new_pivot_idx < len(ul) - 1:
+#            ul[new_pivot_idx+1:], tmp_count_right = \
+#                self.quicksort_choose_last_as_pivot(ul[new_pivot_idx+1:])
+#            comp_count += tmp_count_right
+#
+#        return ul, comp_count
+
+#    def quicksort_choose_median_as_pivot(
 
 
 if __name__ == "__main__":
