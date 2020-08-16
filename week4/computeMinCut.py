cffc341fd2470ddc243a5d12f188edf3395bc72b
Dave Huh <dave.huh11@gmail.com>
2020-08-15 21:16:00 -0500

    completed implementation

--- week4/computeMinCut.py
+++ week4/computeMinCut.py
@@ -11,16 +11,14 @@ class MinCut:
     Compute min cut
     """
 
-    def computeMinCut(self, al, numTrials=10):
+    def computeMinCut(self, graph):
         """
         Compute min cut with adjacent list
         """
 
         # edge cases
-        if al is None:
+        if graph is None:
             sys.exit(1)
-
-        graph = buildList(al)
 
         minCutCount = None
 
@@ -41,12 +39,12 @@ def contractEdge(graph):
     chosenEdge = [graph[chosenNodeIdx][0],
                   graph[chosenNodeIdx][chosenEdgeIdx]]
 
-    print("number of nodes before: ", len(graph))
-    print(chosenNodeIdx, " ", chosenEdgeIdx)
-    print("chosen edge: ", chosenEdge)
-
-    print("before")
-    print(graph[chosenNodeIdx])
+#    print("number of nodes before: ", len(graph))
+#    print(chosenNodeIdx, " ", chosenEdgeIdx)
+#    print("chosen edge: ", chosenEdge)
+#
+#    print("before")
+#    print(graph[chosenNodeIdx])
 
     # add all adjacencies to chosen node
     graph[chosenNodeIdx].pop(chosenEdgeIdx)
@@ -66,13 +64,13 @@ def contractEdge(graph):
     for ele in indexesToRemove:
         graph[chosenNodeIdx].pop(ele)
 
-    print("after")
-    print(graph[chosenNodeIdx])
+#    print("after")
+#    print(graph[chosenNodeIdx])
 
     # pop otherNode
     graph.pop(otherNode)
 
-    print("number of nodes after: ", len(graph))
+#    print("number of nodes after: ", len(graph))
 
     return graph
 
@@ -82,6 +80,7 @@ def buildList(al):
     Build a list from raw adjacency list
     """
     for i in range(len(al)):
+        al[i] = al[i].strip()
         al[i] = list(map(int, al[i].split('\t')))
 
     return al
@@ -94,7 +93,20 @@ if __name__ == "__main__":
 
     txtFile = open(sys.argv[1])
     content_list = txtFile.read().splitlines()
-    min_cut = MinCut()
-    min_cut = min_cut.computeMinCut(content_list)
 
-    print("min cut count: ", min_cut)
+    numTrials = 10000000
+    min_count = None
+
+    min_cut = MinCut()
+
+    graph = buildList(content_list)
+
+    while numTrials > 0:
+        tmp = min_cut.computeMinCut(graph)
+        if min_count is None:
+            min_count = tmp
+        if tmp < min_count:
+            min_count = tmp
+        numTrials -= 1
+
+    print("min cut count: ", min_count)
