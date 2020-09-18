9365c9a1941f467f66681445b081266f4883b61e
Dave Huh <dave.huh11@gmail.com>
2020-09-18 15:19:25 -0500

    completed first iteration of clustering algorithm. Need to optimize by utilizing union-find to get fast access to leader pointers without needing to iterate through all nodes in graph

--- greedy/week2/cluster.py
+++ greedy/week2/cluster.py
@@ -48,43 +48,118 @@ class Cluster:
         self.graph = graph
         self.clusters = []
 
+    def foundMaximumSpacing(self, edge):
+        v1 = edge.v1
+        v2 = edge.v2
+        if v1 in self.clusters and v2 in self.clusters:  # disjoint sets
+            return True
+        elif v1 in self.clusters:
+            for key, values in self.clusters.items():
+                if not values:
+                    continue
+                if key != v1 and v2 in values:
+                    return True
+        elif v2 in self.clusters:
+            for key, values in self.clusters.items():
+                if not values:
+                    continue
+                if key != v2 and v1 in values:
+                    return True
+        else:
+            foundV1 = False
+            foundV2 = False
+            for key, values in self.clusters.items():
+                if not values:
+                    continue
+                if v1 in values and v2 not in values:
+                    foundV1 = True
+                elif v2 in values and v1 not in values:
+                    foundV2 = True
+            return foundV1 and foundV2
+        return False
+
+    def checkSum(self):
+        numberNodes = 0
+        for key, values in self.clusters.items():
+            numberNodes += 1
+            if values:
+                numberNodes += len(values)
+
+        return numberNodes
+
+
     def buildCluster(self):
         edges = self.graph.edges
         self.clusters = dict.fromkeys(self.graph.nodes, None)
-#        print("clusters before: ", self.clusters)
-#       while len(self.clusters) > k:
         while len(edges) > 0:
             edge = heapq.heappop(edges)
             v1 = edge.v1
             v2 = edge.v2
 
+#            print(v1, v2, edge.cost)
+#            print("num of clusters: ", len(self.clusters))
+
             # stopping condition
             if len(self.clusters) == k:
-                if v1 in self.clusters and v2 in self.clusters:
-                    print("final: ", self.clusters)
+#                print("stopping cost: ", edge.cost)
+                stop = self.foundMaximumSpacing(edge)
+                if stop:
+#                    print("final clusters: ", self.clusters)
                     return edge.cost
                 else:
                     continue
 
-            if v1 not in self.clusters or v2 not in self.clusters:
-                continue
-            if v1 in self.clusters and v2 in self.clusters and \
-                    self.clusters[v1] and self.clusters[v2]:
-                self.clusters[v1].add(v2)
-                for key in self.clusters[v2]:
-                    self.clusters[v1].add(key)
-                self.clusters.pop(v2, None)
-                continue
-
-            if v1 in self.clusters:
-                if not self.clusters[v1]:
-                    self.clusters[v1] = {v2}
-                else:
+            if v1 in self.clusters and v2 in self.clusters:
+                if self.clusters[v1]:
                     self.clusters[v1].add(v2)
-                self.clusters.pop(v2, None)
+                else:
+                    self.clusters[v1] = {v2}
 
-        print("final: ", self.clusters)
-        return edges[0].cost
+                if self.clusters[v2]:
+                    self.clusters[v1].update(list(self.clusters[v2]))
+                self.clusters.pop(v2, None)
+            elif v1 in self.clusters:
+                if self.clusters[v1] and v2 in self.clusters[v1]:
+                    continue
+                for key, values in self.clusters.items():
+                    if key != v1 and values and v2 in values:
+                        if self.clusters[v1]:
+                            self.clusters[v1].add(key)
+                            self.clusters[v1].update(list(values))
+                        else:
+                            self.clusters[v1] = {key}
+                            self.clusters[v1].update(list(values))
+                        self.clusters.pop(key, None)
+                        break
+            elif v2 in self.clusters:
+                if self.clusters[v2] and v1 in self.clusters[v2]:
+                    continue
+                for key, values in self.clusters.items():
+                    if key != v2 and values and v1 in values:
+                        if self.clusters[v2]:
+                            self.clusters[v2].add(key)
+                            self.clusters[v2].update(list(values))
+                        else:
+                            self.clusters[v2] = {key}
+                            self.clusters[v2].update(list(values))
+                        self.clusters.pop(key, None)
+                        break
+            else:
+                v1Key = -1
+                v2Key = -1
+                for key, values in self.clusters.items():
+                    if values and v1 in values:
+                        v1Key = key
+                    if values and v2 in values:
+                        v2Key = key
+                if v1Key != v2Key:
+                    self.clusters[v1Key].add(v2Key)
+                    self.clusters[v1Key].update(list(self.clusters[v2Key]))
+                    self.clusters.pop(v2Key)
+
+#            print("end clusters: ", self.clusters)
+
+        return edge.cost
 
 
 if __name__ == "__main__":
@@ -95,11 +170,14 @@ if __name__ == "__main__":
     textFile = open(sys.argv[1])
     textFile = textFile.read().splitlines()
     numNodes = textFile.pop(0)
-    list_graph = [list(map(int, ele.split(" "))) for ele in textFile]
+    list_graph = [list(map(int, ele.strip().split(" "))) for ele in textFile]
     graph = Graph()
     graph.buildGraph(list_graph)
 
     k = 4  # a priori number of clusters
     clusters = Cluster(k, graph)
     max_spacing = clusters.buildCluster()
+    checkSum = clusters.checkSum()
+    print("k: ", k)
+    print("check sum: ", numNodes, "=? ", checkSum)
     print("max spacing: ", max_spacing)
