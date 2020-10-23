45b801ce8f3227365e7382f0dfa9153ebf894aca
davehuh <dave.huh11@gmail.com>
2020-10-22 22:07:48 -0500

    naive implementation of mwis

--- greedy/week3/maxweight/mw.py
+++ greedy/week3/maxweight/mw.py
@@ -12,6 +12,16 @@ class Node:
         self.val = val
         self.weight = weight
 
+    def __add__(self, other):
+        return self.weight + other
+
+    def __radd__(self, other):
+        return other + self.weight
+
+    def getVal(self):
+        return self.val
+
+
 class Graph:
     def __init__(self):
         self.nodes = []
@@ -25,8 +35,20 @@ class Graph:
             i += 1
 
     def computeMWIS(self):
-        set1 = self.nodes[0::2]
-        set2 = self.nodes[1::2]
+        """
+        Computes maximum-weight independent set
+        returns maximum weight and M-W set
+        """
+        set1 = np.array(self.nodes[0::2])
+        set2 = np.array(self.nodes[1::2])
+
+        sumSet1 = set1.sum()
+        sumSet2 = set2.sum()
+
+        if sumSet1 > sumSet2:
+            return set1, sumSet1
+
+        return set2, sumSet2
 
 
 if __name__ == "__main__":
@@ -41,3 +63,19 @@ if __name__ == "__main__":
 
     graph = Graph()
     graph.buildNodes(vert_weights)
+    maxSet, maxWeight = graph.computeMWIS()
+
+    testNodes = [1,2,3,4,17,117,517,997]
+    bits = ''
+
+    vecfunc = np.vectorize(Node.getVal, otypes=[object])
+    vals = set(vecfunc(maxSet))
+
+    for test in testNodes:
+        if test in vals:
+            bits = bits + '1'
+        else:
+            bits = bits + '0'
+
+    print("weight: ", maxWeight)
+    print("bits: ", bits)
