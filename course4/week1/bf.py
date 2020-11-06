f87482074fcce90fe0786e86db39f2a6f0bb19b6
davehuh <dave.huh11@gmail.com>
2020-11-05 21:03:07 -0600

    n bellmanford implementation, still incorrect solution with graph with no negative cycles

--- course4/week1/bf.py
+++ course4/week1/bf.py
@@ -20,7 +20,8 @@ class Graph:
     def __init__(self, vl):
         self.vl = vl
         self.dict = {}
-        self.numEdges = len(vl)
+        self.num_edges = len(vl)
+        self.vertices_set = set()
 
     def build_graph(self):
         """
@@ -38,6 +39,9 @@ class Graph:
             else:
                 self.dict[tail] = [edge]
 
+            # add vertices to ref set
+            self.vertices_set.update([tail, head])
+
 
 class BellmanFord:
     """
@@ -45,13 +49,12 @@ class BellmanFord:
     between two vertices in a graph for a general case
     without assumption of negative edge lengths and cycles
     """
-    def __init__(self, edgeList, graph, numVert, numEdges):
+    def __init__(self, edgeList, graph, num_vert, num_edges):
         self.el = edgeList
         self.graph = graph
-        self.numVert = numVert
-        self.numEdges = numEdges
-        self.matrix = np.zeros((numEdges, numVert))
-        self._buildMatrix()
+        self.num_vert = num_vert
+        self.num_edges = num_edges
+        self.matrix = np.zeros((num_edges, num_vert))
 
     def _buildMatrix(self):
         """
@@ -99,8 +102,8 @@ class BellmanFord:
 
         penUltVert_Dict = {}  # w (penultimate) vertices
 
-        for edge in range(1, self.numEdges):
-            for vertex in range(self.numVert):
+        for edge in range(1, self.num_edges):
+            for vertex in range(self.num_vert):
                 case1 = self.matrix[edge-1, vertex]
 
                 case2 = np.inf
@@ -113,6 +116,48 @@ class BellmanFord:
 
                 self.matrix[edge, vertex] = min(case1, case2)
 
+    def compute_shortest_path(self, source):
+        """
+        copmutes shortest path between source and destination
+        """
+        self._buildMatrix()
+        self.matrix[0, source-1] = 0
+
+        vertex_queue = deque([source])
+
+        min_distance = np.inf
+
+        for num_edge in range(1, self.num_edges):
+            if not vertex_queue:
+                break
+
+            vertex = vertex_queue.pop()
+
+            vertex_edges = []
+            if vertex in self.graph.dict:
+                vertex_edges = self.graph.dict[vertex]
+
+            for edge in vertex_edges:
+                head = edge.head
+                tail = edge.tail
+                cost = edge.length
+
+                case1 = self.matrix[num_edge-1, head-1]
+
+                # TODO tail is v not w
+                case2 = self.matrix[num_edge-1, tail-1] + cost
+
+                minCase1Case2 = min(case1, case2)
+
+                if minCase1Case2 < min_distance:
+                    min_distance = minCase1Case2
+
+                self.matrix[num_edge, head-1] = minCase1Case2
+
+                vertex_queue.appendleft(head)
+
+        return min_distance
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -123,16 +168,27 @@ if __name__ == "__main__":
     file = [row.split(' ') for row in file]
     vertices_lengths = [list(map(int, row)) for row in file]
 
-    numVert, numEdges = vertices_lengths.pop(0)
+    num_vert, num_edges = vertices_lengths.pop(0)
 
     s = 1
     v = 3
 
     graph_ = Graph(vertices_lengths)
     graph_.build_graph()
+
 #
 #    print(graph_.dict)
 #
-#    bf = BellmanFord(graph_.dict, numVert, numEdges)
-    bf = BellmanFord(vertices_lengths, graph_, numVert, numEdges)
-    bf.computeShortestPath(s, v)
+#    bf = BellmanFord(graph_.dict, num_vert, num_edges)
+#    bf = BellmanFord(vertices_lengths, graph_, num_vert, num_edges)
+#    bf.computeShortestPath(s)
+
+    min_dist = np.inf
+
+    vertices = graph_.vertices_set
+
+    for vertex in vertices:
+        bf = BellmanFord(vertices_lengths, graph_, num_vert, num_edges)
+        min_dist = min(min_dist, bf.compute_shortest_path(vertex))
+
+    print(min_dist)
